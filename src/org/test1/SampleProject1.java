ba7f48877147022671731f1421c94247ce2decf6
SeleniumGit63 <sathishamutha46@gmail.com>
2020-02-20 18:56:05 +0530

    Sathish Added 3 Lines

--- src/org/test1/SampleProject1.java
+++ src/org/test1/SampleProject1.java
@@ -4,5 +4,8 @@ public class SampleProject1 {
 	public static void main(String[] args) {
 		System.out.println("Java");
 		System.out.println("Application");
+		System.out.println("Sathish Added Line 1");
+		System.out.println("Sathish added line 2");
+		System.out.println("Sathish added Line 3");
 	}
 }
